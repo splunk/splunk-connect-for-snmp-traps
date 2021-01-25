342a9a0b8b7cce935cb93f4cebb0dc405850ba69
lstoppa <lstoppa@splunk.com>
2021-01-25 10:37:29 +0100

    removed server logic from __init__.py - now the root __init__.py file is used only for global logging initializations - Python suggests to use single quotes instead of double-quotes for string - added a utilities.py file (for now we placed in there all the signal logic) - moved all snmp server logic to manager.trap_server.py - added splunk_connect_for_smnp_trap.py as the main application entry point
    You can test it in this way:
    - run the server, and from your command line run (the server listens by
      default on port 2062)
    - snmptrap -v 2c -c public 127.0.0.1:2062 "" 1.3.6.1.4.1.6141.2.60.37.0.2 1.3.6.1.4.1.6141.2.60.37.1.1.11.1.1.1 t 14096763

---
+++ splunk_connect_for_snmp_traps/splunk_connect_for_smnp_trap.py
@@ -0,0 +1,43 @@
+import logging.config
+import argparse
+import yaml
+from splunk_connect_for_snmp_traps.manager.trap_server import trap_server
+from utilities import initialize_signals_handler
+
+logger = logging.getLogger(__name__)
+
+
+def main():
+    logger.info(f'Startup Config')
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-l',
+        '--loglevel',
+        default='info',
+        help='Provide logging level. Example --loglevel debug, default=warning',
+    )
+    parser.add_argument(
+        '-p', '--port', default='2062', help='Port used to accept traps', type=int
+    )
+    parser.add_argument('-c', '--config', default='config.yaml', help='Config File')
+    args = parser.parse_args()
+
+    log_level = args.loglevel.upper()
+    config_file = args.config
+
+    logging.getLogger().setLevel(log_level)
+    logger.info(f'Log Level is {log_level}')
+    logger.info(f'Config file is {config_file}')
+
+    logger.info('Completed Argument parsing')
+
+    with open(config_file, 'r') as yamlfile:
+        server_config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+    logger.debug(f'Server Config is:  {server_config}')
+    trap_server(port=args.port, server_config=server_config)
+
+
+if __name__ == '__main__':
+    initialize_signals_handler()
+    main()
