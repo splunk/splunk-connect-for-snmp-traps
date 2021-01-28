d344626cf50804c3a917c4912ee50369d177822b
lstoppa <77723162+lstoppa@users.noreply.github.com>
2021-01-28 18:25:31 +0100

    Feature/change logging structure (#16)
    * removed server logic from __init__.py
    - now the root __init__.py file is used only for global logging initializations
    - Python suggests to use single quotes instead of double-quotes for string
    - added a utilities.py file (for now we placed in there all the signal logic)
    - moved all snmp server logic to manager.trap_server.py
    - added splunk_connect_for_smnp_trap.py as the main application entry point
    
    You can test it in this way:
    - run the server, and from your command line run (the server listens by
      default on port 2062)
    - snmptrap -v 2c -c public 127.0.0.1:2062 "" 1.3.6.1.4.1.6141.2.60.37.0.2 1.3.6.1.4.1.6141.2.60.37.1.1.11.1.1.1 t 14096763
    
    * Added HEC configuration file
    - added a section for protocol, host, port, endpoint, and token that will be used
      to perform REST calls to HEC
    
    * Added basic communication to HEC
    
    - removed the extra --port command line parameter and put it into config.yaml file
    - added a basic functionality to send POST requests to HEC
    
    * Changes for https://github.com/splunk/splunk-connect-for-snmp-traps/pull/16
    
    * Added support for multithreading when sending requests to HEC
    - added support for ThreadPoolExecutor
    - added unit-tests

---
+++ splunk_connect_for_snmp_traps/utilities.py
@@ -0,0 +1,18 @@
+import logging
+import signal
+
+logger = logging.getLogger(__name__)
+
+
+def default_signal_handler(signal_number, frame):
+    logger.info(f'Received Signal: {signal_number}')
+    return
+
+
+def initialize_signals_handler():
+    signals_to_catch = (
+        signal.SIGHUP, signal.SIGINT, signal.SIGQUIT, signal.SIGQUIT, signal.SIGILL, signal.SIGTRAP, signal.SIGABRT,
+        signal.SIGBUS, signal.SIGFPE, signal.SIGUSR1, signal.SIGSEGV, signal.SIGUSR2, signal.SIGPIPE, signal.SIGALRM,
+        signal.SIGTERM)
+    for one_signal in signals_to_catch:
+        signal.signal(one_signal, default_signal_handler)
