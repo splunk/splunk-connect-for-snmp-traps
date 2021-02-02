c29080568bbbacd2de0a5988780bf3e59ffaf408
rfaircloth-splunk <rfaircloth@splunk.com>
2021-02-02 15:09:49 -0500

    rebase and reformat

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,18 @@
 FROM registry.access.redhat.com/ubi8/ubi
 
+WORKDIR /work
+
 RUN curl -fsSL https://goss.rocks/install | GOSS_VER=v0.3.13 sh
 
 RUN cd /tmp ;\
     dnf install tzdata curl wget nc python3.8 python3-pip procps-ng -y ;\
-    dnf update -y ;\
-    mkdir /work
+    dnf update -y
 
-COPY entrypoint.sh /
+COPY lookups ./lookups
+COPY entrypoint.sh .
 COPY dist/*.whl /tmp
-COPY config.yaml /
+COPY config.yaml .
 RUN pip3.8 install $(ls /tmp/*.whl); rm -f /tmp/*.whl
 
-EXPOSE 612/udp
-ENTRYPOINT [ "/entrypoint.sh" ]
+EXPOSE 2162/udp
+ENTRYPOINT [ "/work/entrypoint.sh" ]
--- splunk_connect_for_snmp_traps/manager/trap_server.py
+++ splunk_connect_for_snmp_traps/manager/trap_server.py
@@ -7,7 +7,15 @@ from pysnmp.entity import engine, config
 from pysnmp.entity.rfc3413 import ntfrcv
 
 from splunk_connect_for_snmp_traps.manager.hec_config import HecConfiguration
-from splunk_connect_for_snmp_traps.manager.os_config_utils import max_allowed_working_threads
+from splunk_connect_for_snmp_traps.manager.os_config_utils import (
+    max_allowed_working_threads,
+)
+from splunk_connect_for_snmp_traps.manager.translator import Translator
+
+from pysnmp.smi import builder, view, compiler, rfc1902
+import os
+import json
+import csv
 
 logger = logging.getLogger(__name__)
 
@@ -19,73 +27,101 @@ class TrapServer:
         self.configure_trap_server()
         self._hec_config = HecConfiguration()
         self._thread_pool_executor = self.configure_thread_pool()
+        self._translator = Translator(server_config)
 
     def configure_thread_pool(self):
-        user_suggested_working_threads = self._server_config['thread-pool']['max-suggested-working-threads']
+        user_suggested_working_threads = self._server_config["thread-pool"][
+            "max-suggested-working-threads"
+        ]
         max_workers = max_allowed_working_threads(user_suggested_working_threads)
-        logger.debug(f'Configured a thread-pool with {max_workers} concurrent threads')
+        logger.debug(f"Configured a thread-pool with {max_workers} concurrent threads")
         return concurrent.futures.ThreadPoolExecutor(max_workers=max_workers)
 
     def configure_trap_server(self):
-        self._snmp_engine.observer.registerObserver(self.request_observer, 'rfc3412.receiveMessage:request',
-                                                    'rfc3412.returnResponsePdu')
-        snmp_config = self._server_config['snmp']
-        if snmp_config['ipv4']:
-            config.addTransport(self._snmp_engine, udp.domainName,
-                                udp.UdpTransport().openServerMode(('127.0.0.1', snmp_config['port'])))
-        if snmp_config['ipv6']:
-            config.addTransport(self._snmp_engine, udp6.domainName,
-                                udp6.Udp6Transport().openServerMode(('::1', snmp_config['port'])))
+        self._snmp_engine.observer.registerObserver(
+            self.request_observer,
+            "rfc3412.receiveMessage:request",
+            "rfc3412.returnResponsePdu",
+        )
+        snmp_config = self._server_config["snmp"]
+        if snmp_config["ipv4"]:
+            config.addTransport(
+                self._snmp_engine,
+                udp.domainName,
+                udp.UdpTransport().openServerMode(("127.0.0.1", snmp_config["port"])),
+            )
+        if snmp_config["ipv6"]:
+            config.addTransport(
+                self._snmp_engine,
+                udp6.domainName,
+                udp6.Udp6Transport().openServerMode(("::1", snmp_config["port"])),
+            )
         # SNMPv1/2c setup
         # SecurityName <-> CommunityName mapping
-        for community in snmp_config['communities']['v1']:
-            logger.info(f'Configuring V1 {community}')
+        for community in snmp_config["communities"]["v1"]:
+            logger.info(f"Configuring V1 {community}")
             config.addV1System(self._snmp_engine, community, community)
         # Register SNMP Application at the SNMP engine
         ntfrcv.NotificationReceiver(self._snmp_engine, self.snmp_callback_function)
 
     def post_trap_to_hec(self, variables_binds):
-        logger.debug('Task received, sleeping for few seconds ...')
+        logger.debug("Task received, sleeping for few seconds ...")
         for endpoint in self._hec_config.get_endpoints():
-            headers = {'Authorization': f'Splunk {self._hec_config.get_authentication_token()}'}
-            splunk_trap_data = ','.join([str(key) + str(value) for key, value in variables_binds])
-            data = {'sourcetype': 'trap-server', 'event': splunk_trap_data}
-            logger.debug(f'Posting trap to HEC using {endpoint} and headers {headers}')
-            response = requests.post(url=endpoint, json=data, headers=headers, verify=self._hec_config.is_ssl_enabled())
-            logger.debug(f'Response code is {response.status_code}')
+            headers = {
+                "Authorization": f"Splunk {self._hec_config.get_authentication_token()}"
+            }
+            data = {"sourcetype": "trap-server", "event": variables_binds}
+            logger.debug(f"Posting trap to HEC using {endpoint} and headers {headers}")
+            response = requests.post(
+                url=endpoint,
+                json=data,
+                headers=headers,
+                verify=self._hec_config.is_ssl_enabled(),
+            )
+            logger.debug(f"Response code is {response.status_code}")
 
     # Register a callback to be invoked at specified execution point of
     # SNMP Engine and passed local variables at code point's local scope
     # noinspection PyUnusedLocal,PyUnusedLocal
     def request_observer(self, snmp_engine, execution_point, variables, callback_ctx):
         logger.debug(f'Raw data is "{variables}"')
-        logger.debug('Execution point: %s' % execution_point)
+        logger.debug("Execution point: %s" % execution_point)
         logger.debug(
-            '* transportDomain: %s'
-            % '.'.join([str(x) for x in variables['transportDomain']])
+            "* transportDomain: %s"
+            % ".".join([str(x) for x in variables["transportDomain"]])
         )
         logger.debug(
-            '* transportAddress: %s'
-            % '@'.join([str(x) for x in variables['transportAddress']])
+            "* transportAddress: %s"
+            % "@".join([str(x) for x in variables["transportAddress"]])
         )
-        logger.debug('* securityModel: %s' % variables['securityModel'])
-        logger.debug('* securityName: %s' % variables['securityName'])
-        logger.debug('* securityLevel: %s' % variables['securityLevel'])
-        logger.debug('* contextEngineId: %s' % variables['contextEngineId'].prettyPrint())
-        logger.debug('* contextName: %s' % variables['contextName'].prettyPrint())
-        logger.debug('* PDU: %s' % variables['pdu'].prettyPrint())
+        logger.debug("* securityModel: %s" % variables["securityModel"])
+        logger.debug("* securityName: %s" % variables["securityName"])
+        logger.debug("* securityLevel: %s" % variables["securityLevel"])
+        logger.debug(
+            "* contextEngineId: %s" % variables["contextEngineId"].prettyPrint()
+        )
+        logger.debug("* contextName: %s" % variables["contextName"].prettyPrint())
+        logger.debug("* PDU: %s" % variables["pdu"].prettyPrint())
 
     # Callback function for receiving notifications
     # noinspection PyUnusedLocal,PyUnusedLocal,PyUnusedLocal
-    def snmp_callback_function(self, snmp_engine, state_reference, context_engine_id, context_name, var_binds,
-                               callback_ctx):
+    def snmp_callback_function(
+        self,
+        snmp_engine,
+        state_reference,
+        context_engine_id,
+        context_name,
+        var_binds,
+        callback_ctx,
+    ):
         logger.debug(
             'Notification from ContextEngineId "%s", ContextName "%s"'
             % (context_engine_id.prettyPrint(), context_name.prettyPrint())
         )
-        for name, val in var_binds:
-            logger.debug('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
-        self._thread_pool_executor.submit(self.post_trap_to_hec, var_binds)
+        # Translate the var_binds to MIB objects
+        trap_event_string = self._translator.format_trap_event(var_binds)
+
+        self._thread_pool_executor.submit(self.post_trap_to_hec, trap_event_string)
 
     def run_trap_server(self):
         self._snmp_engine.transportDispatcher.jobStarted(1)
