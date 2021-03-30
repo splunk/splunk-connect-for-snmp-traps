9a87b0349e236b9a5783fbfb65f9e8d21c3eeb65
weliasz <weliasz@splunk.com>
2021-03-30 15:29:17 +0200

    feat: reverting main change

---
+++ splunk_connect_for_snmp_traps/manager/hec_config.py
@@ -0,0 +1,43 @@
+import logging
+import os
+
+logger = logging.getLogger(__name__)
+
+
+class HecConfiguration:
+    endpoints_env_variable_name = "SPLUNK_HEC_URL"
+    authentication_token_env_variable_name = "SPLUNK_HEC_TOKEN"
+    enable_ssl_env_variable_name = "SPLUNK_HEC_TLS_VERIFY"
+
+    def __init__(self):
+        urls = os.environ.get(HecConfiguration.endpoints_env_variable_name)
+        if urls is None:
+            raise ValueError(
+                f"{HecConfiguration.endpoints_env_variable_name} environment variable undefined"
+            )
+        self._urls_list = urls.split()
+
+        authentication_token = os.environ.get(
+            HecConfiguration.authentication_token_env_variable_name
+        )
+        if authentication_token is None:
+            raise ValueError(
+                f"{HecConfiguration.authentication_token_env_variable_name} environment variable undefined"
+            )
+        self._authentication_token = authentication_token
+
+        enable_ssl = os.environ.get(HecConfiguration.enable_ssl_env_variable_name)
+        if enable_ssl is None:
+            raise ValueError(
+                f"{HecConfiguration.enable_ssl_env_variable_name} environment variable undefined"
+            )
+        self._enable_ssl = True if enable_ssl.lower() == "yes" else False
+
+    def get_endpoints(self):
+        return self._urls_list
+
+    def get_authentication_token(self):
+        return self._authentication_token
+
+    def is_ssl_enabled(self):
+        return self._enable_ssl
