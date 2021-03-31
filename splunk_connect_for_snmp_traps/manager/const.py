2afba19a1cea4acbc7cad29ae531beba31576935
Yuan Ling <yling@splunk.com>
2021-03-31 16:04:52 -0700

    fix: added noAuth type and noPriv type for AuthProtocol and privProtocol respectively

--- splunk_connect_for_snmp_traps/manager/const.py
+++ splunk_connect_for_snmp_traps/manager/const.py
@@ -1,11 +1,13 @@
 from pysnmp.entity import config
 
 AuthProtocolMap = {
+    "NOAUTH": config.usmNoAuthProtocol,
     "MD5": config.usmHMACMD5AuthProtocol,
     "SHA": config.usmHMACSHAAuthProtocol
 }
 
 PrivProtocolMap = {
+    "NOPRIV": config.usmNoPrivProtocol,
     "DES": config.usmDESPrivProtocol,
     "3DES": config.usm3DESEDEPrivProtocol,
     "AES": config.usmAesCfb128Protocol,
