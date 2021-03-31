c2bfae0168cf972ead500fe3964f24e83eacffa3
Pawel Szkamruk <pszkamruk@splunk.com>
2021-03-31 13:10:15 +0200

    updates

--- tests/perfromance/send_trap.py
+++ tests/perfromance/send_trap.py
@@ -28,7 +28,6 @@ def send_trap(host, port, object_identity, *var_binds):
 
 
 def send_trap_loop(shared_value_between_processes, log_dir, duration_sec, name):
-    # def send_trap_loop(log_dir, duration_sec, name):
     index = 0
     start = time.time()
     while True:
@@ -42,21 +41,8 @@ def send_trap_loop(shared_value_between_processes, log_dir, duration_sec, name):
         #     break
         if time.time() - start > duration_sec:
             shared_value_between_processes.value = shared_value_between_processes.value + index
-            print(f"Saving no.of traps send for {name}")
+            print(f"Saving no.of traps send for {name}: {index}")
             utils.save_data(log_dir + name + ".txt", index)
             break
     return index
 
-#
-# if __name__ == '__main__':
-#     # send trap
-#     start_time = time.time()
-#     # varbind1 = ('1.3.6.1.6.3.1.1.4.3.0', '1.3.6.1.4.1.20408.4.1.1.2')
-#     # varbind2 = ('1.3.6.1.2.1.1.1.0', OctetString('my system'))
-#     # send_trap("10.202.2.209", 162, '1.3.6.1.6.3.1.1.5.2', varbind1, varbind2)
-#
-#     how_many = send_trap_loop(10, "test")
-#     duration = time.time() - start_time
-#     print(f"Sending event took: {duration}, no. of messages: {how_many}")
-#     print(f"time needed to send single event: {duration / how_many}")
-#     print(f"events per sec: {how_many / duration}")
