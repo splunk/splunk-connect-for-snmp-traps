0fa1acd1075542d73e97321b6b820a306220ba49
lstoppa <lstoppa@splunk.com>
2021-05-26 14:36:03 +0200

    fix: added 2021 copyrights

--- splunk_connect_for_snmp_traps/manager/trap_server.py
+++ splunk_connect_for_snmp_traps/manager/trap_server.py
@@ -1,23 +1,33 @@
+#   ########################################################################
+#   Copyright 2021 Splunk Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#   ########################################################################
 import logging
-
-from pysnmp.carrier.asyncore.dgram import udp, udp6
-from pysnmp.entity import engine, config
-from pysnmp.entity.rfc3413 import ntfrcv
-
-from pysnmp.proto.secmod.rfc3826.priv import aes
-from pysnmp.proto.secmod.rfc3414.auth import hmacsha
-from pysnmp.proto import rfc1902
-
-from splunk_connect_for_snmp_traps.manager.hec_sender import HecSender
-from splunk_connect_for_snmp_traps.manager.mib_server_client import get_translation
-from splunk_connect_for_snmp_traps.manager.const import AuthProtocolMap, PrivProtocolMap
-import socket
 import os
-
+import socket
 
 # *TODO*: enable debug all only if end-user has set debug logging mode.
 # debugging log for SNMPv3 trap
 from pysnmp import debug
+from pysnmp.carrier.asyncore.dgram import udp, udp6
+from pysnmp.entity import engine, config
+from pysnmp.entity.rfc3413 import ntfrcv
+from pysnmp.proto import rfc1902
+
+from splunk_connect_for_snmp_traps.manager.const import AuthProtocolMap, PrivProtocolMap
+from splunk_connect_for_snmp_traps.manager.hec_sender import HecSender
+from splunk_connect_for_snmp_traps.manager.mib_server_client import get_translation
 
 debug.setLogger(debug.Debug("all"))
 
